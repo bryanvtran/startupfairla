a39f511256b7df42582e0548df1251b4ca9d7615
Bryan Tran <bryanvantran@ucla.edu>
2016-07-27 23:02:18 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict";
+    new WOW().init();
+
+    $('a.page-scroll').bind('click', function(event) {
+        var $ele = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($ele.attr('href')).offset().top - 60)
+        }, 1450, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+    $('#galleryModal').on('show.bs.modal', function (e) {
+       $('#galleryImage').attr("src",$(e.relatedTarget).data("src"));
+    });
+
+})(jQuery);
